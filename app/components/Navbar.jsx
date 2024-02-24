dc0cb09fb2b13c0cc24d4732797ed48101417bda
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-02-24 18:45:49 +0100

    Updated Navbar title and wrapped it in a link to homepage for improved navigation and branding clarity

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,9 +3,11 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav>
+      <Link href="/">
       <h1>
-        Serwer Expert Ticket
-      </h1>
+          Serwer Expert HD
+       </h1>
+      </Link>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
   </nav>
