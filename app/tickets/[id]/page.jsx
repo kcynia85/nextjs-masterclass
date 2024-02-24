0f35f347147149f1af30eb32c57601005b685e8f
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-02-24 16:24:23 +0100

    Added ticket listing and detail views with dynamic data fetching

---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+const getTicket = async (id) => {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60 // use 0 to opt out of using cache
+    }
+  });
+
+  return res.json();
+}
+
+const TicketDeatils = async ({ params }) => {
+  const ticket = await getTicket(params.id);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">  
+      <h3>{ticket.title}</h3>
+      <p>{ticket.body}</p>
+      <div className={`pill ${ticket.priority}`}>
+        {ticket.priority} priority
+        </div>
+        <div className="flex justify-center my-8">
+          <Link href={`/tickets/${ticket.id}/edit`}>
+            <button className="btn-primary">Edit Ticket</button>
+          </Link>
+          <Link href="/tickets">
+            <button className="btn-primary">Back to Tickets</button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default TicketDeatils;
